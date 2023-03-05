11b56ef6576d329f9c8475a15e84b5cf6b0ee3de
Arvind Kumar <arvindkumar2575@gmail.com>
2023-03-05 12:03:28 +0530

    first commit

---
+++ public/assets/data.js
@@ -0,0 +1,88 @@
+const d = {
+    s:[
+        'Java',
+        'JavaScript',
+        'JQuery',
+
+        'HTML',
+        'LESS',
+        'CSS',
+        'Bootstrap',
+        
+
+        'NodeJs',
+        'ExpressJs',
+        'ReactJs',
+
+        'PHP',
+        'CodeIgnitor',
+        'Laravel',
+
+        'Adobe Experience Manager',
+        'Components Development',
+        'Slightly/HTL',
+        'Servlet',
+        'Services',
+        'Postman',
+        'JIRA',
+        'Confluence Tool',
+        'Jenkins',
+        'GIT',
+        'GitHub',
+        'BitBucket',
+
+        'Materialize',
+        'API Development',
+        'MySQL',
+        'MongoDB',
+        'ClickUp',
+        'Docker',
+
+        'Excel',
+        'Google Spreadsheet',
+        'Data Analysis'
+    ],
+    sp:[
+        ['Java',80],
+        ['PHP',90]
+        ['JavaScript',100],
+        ['JQuery',95],
+        ['Python',60],
+        ['HTML & CSS',95],
+        ['SQL',80],
+        ['MongoDB',60],
+        ['NodeJS',75],
+        ['ExpressJs',70],
+        ['ReactJs',50]
+    ],
+    e:[
+        {
+            image:'./assets/images/infosys-logo-1.png',
+            designation:'Senior Systems Engineer',
+            company_name:'Infosys Limited',
+            loc:'Gurgaon',
+            description:'Working on AEM as AEM Developer and developing components, templates, workflows, sling models & services, other product integration and, work on CRXDE Editor, OSGi configuration, core component, ACS Common, Adobe I/O Runtime, ',
+            tenure:['Present','1 Year'],
+            skill:['Java','JavaScript','JQuery','HTML','LESS','CSS','NodeJs','Adobe Experience Manager','Components Development','Slightly/HTL','Servlet','Services','Postman','JIRA','Confluence Tool','Jenkins','GIT','GitHub','BitBucket']
+        },
+        {
+            image:'./assets/images/techjockey-logo-1.svg',
+            designation:'Software Developer',
+            company_name:'Techjockey Infotech Pvt Ltd',
+            loc:'New Delhi',
+            description:'Worked as Software Developer, developed new websites, code in php framework for adding new features in projects, create apis using nodejs, libraries using javascript, integrate those with exiting project. Website debugging, code optimization, web performance along with code reviews, ',
+            tenure:['Worked','10 Months'],
+            skill:['PHP','JavaScript','JQuery','HTML','LESS','CSS','NodeJs','ExpressJs','Codeignitor','Laravel','ReactJs','Bootstrap','Materialize','API Development','MySQL','MongoDB','Postman','JIRA','ClickUp','GIT','GitHub','Docker']
+        },
+        {
+            image:'./assets/images/buzzyears-edu-logo-1.png',
+            designation:'Implementation Executive',
+            company_name:'Buzzyears Education Pvt Ltd',
+            loc:'Gurgaon',
+            description:'Worked on designing of UI using HTML, CSS, JavaScript, and JQuery functions, write js functions, create calculation workflow & data pipelines using internal application. Collect client requirement and worked on jira. Create report using SQL queries and intergrate with internal application, ',
+            tenure:['Worked','1 Year 6 Months'],
+            skill:['JavaScript','JQuery','HTML','CSS','Bootstrap','MySQL','JIRA','Excel','Google Spreadsheet','Data Analysis']
+        }
+    ]
+
+}
---
+++ public/assets/js/main.js
@@ -0,0 +1,88 @@
+let app = {}
+let templates = {}
+let data = d
+
+app.skillRandomColor = (s) => {
+    let a = {}
+    s.forEach(e => {
+        a["" + e + ""] = "" + app.randomColor(1, 225) + ',' + app.randomColor(1, 225) + ',' + app.randomColor(1, 225) + ""
+    })
+    return a
+}
+app.randomColor = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min
+}
+// get skill colors 
+const skills = app.skillRandomColor(data.s)
+
+// get experience skills colors 
+app.expSkills = (ms, as) => {
+    let a = []
+    for (const [key, value] of Object.entries(ms)) {
+        let o = {}
+        o["" + value + ""] = as["" + value + ""]
+        a.push(o)
+    }
+    return a
+}
+
+// let s1 = data.e[0].skill
+// let s2 = d.e[1].skill
+// let dddd = app.expSkills(s1, skills)
+// console.log(templates.ms(dddd))
+// console.log(app.expSkills(s2,skills))
+
+
+window.addEventListener('load', () => {
+    let expDiv = document.getElementById("id-AR3312")
+    let exp = data.e
+    let etv = ''
+    exp.forEach(i => {
+        i.skill = app.expSkills(i.skill, skills)
+        i.ms=templates.ms(i.skill)
+        etv += templates.e(i)
+    })
+    expDiv.innerHTML = etv
+});
+
+
+templates.e = (d) => {
+    return `
+    <div class="col-md-12 experience-row-JX">
+        <div class="row">
+            <div class="col-md-12 experience-JX">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="media">
+                            <img src="${d.image}" class="mr-3 AR3318-JX" alt="...">
+                            <div class="media-body">
+                                <h6 class="mt-0 mb-0 position-role-JX">
+                                    <span>${d.designation}</span>
+                                    <span class="tenure-JX">${d.tenure[0]} : ${d.tenure[1]}</span>
+                                </h6>
+                                <div class="company-name-location-JX"><span>${d.company_name} - ${d.loc}</span>
+                                </div>
+                                <div class="work-description-JX"><span class="font-weight-bold">Description</span> : ${d.description}<a href="#">read more...</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 tools-tech-JX">
+            <ul>${d.ms}</ul>
+        </div>
+    </div>
+    `
+}
+templates.ms = (d) => {
+    let s = ''
+    for (const [key, value] of Object.entries(d)) {
+        for (const [vkey, vvalue] of Object.entries(value)) {
+            s += `<li data-sindex="${key}" style="background:rgb(${vvalue})">${vkey}</li>`
+        }
+    }
+    return s
+}
+
+
