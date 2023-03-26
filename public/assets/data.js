4c81f6711b228d1a8ad32553e770f62fd5396400
Arvind Kumar <arvindkumar2575@gmail.com>
2023-03-26 18:38:35 +0530

    add experience,project details,model functionality

--- public/assets/data.js
+++ public/assets/data.js
@@ -9,7 +9,8 @@ const d = {
         'CSS',
         'Bootstrap',
         
-
+        'Maven',
+        'Spring Boot',
         'NodeJs',
         'ExpressJs',
         'ReactJs',
@@ -18,7 +19,7 @@ const d = {
         'CodeIgnitor',
         'Laravel',
 
-        'Adobe Experience Manager',
+        'Adobe Experience Manager(AEM)',
         'Components Development',
         'Slightly/HTL',
         'Servlet',
@@ -43,14 +44,17 @@ const d = {
         'Data Analysis'
     ],
     sp:[
-        ['Java',80],
+        ['Java',90],
         ['PHP',90],
         ['JavaScript',100],
-        ['JQuery',95],
         ['Python',60],
+        
+        ['JQuery',95],
         ['HTML & CSS',95],
+
         ['SQL',80],
         ['MongoDB',60],
+        
         ['NodeJS',75],
         ['ExpressJs',70],
         ['ReactJs',50]
@@ -63,7 +67,25 @@ const d = {
             loc:'Gurgaon',
             description:'Working on AEM as AEM Developer and developing components, templates, workflows, sling models & services, other product integration and, work on CRXDE Editor, OSGi configuration, core component, ACS Common, Adobe I/O Runtime, ',
             tenure:['Present','1 Year'],
-            skill:['Java','JavaScript','JQuery','HTML','LESS','CSS','NodeJs','Adobe Experience Manager','Components Development','Slightly/HTL','Servlet','Services','Postman','JIRA','Confluence Tool','Jenkins','GIT','GitHub','BitBucket']
+            skill:['Java','Spring Boot','Maven','JavaScript','JQuery','HTML','LESS','CSS','NodeJs','Adobe Experience Manager(AEM)','Components Development','Slightly/HTL','Servlet','Services','Postman','JIRA','Confluence Tool','Jenkins','GIT','GitHub','BitBucket'],
+            projects:[
+                'MerckGroup Healthcare Projects',
+            ],
+            points:[
+                'Components, services(business-logic), servlets development & clientlib setup for web',
+                'Sling models, OSGI configuration, JCR queries(XPATH,SQL2) for AEM projects(Maven project)',
+                'Built & integrate RESTful API using spring framework(spring boot) & H2 database',
+                'Built & deploy nodejs applications in docker containers and give control over using pm2',
+                'Third party integration - API- Cowin, Ibond, Veeva, Clinicaltrial & OneTrust',
+                'Testing of apis using postman tool',
+                'Create & use jenkins jobs for deployment of code on development server',
+                'Jira & confluence pages use for project management',
+                'Website optimization upto 23% which increase active users',
+                'Carrying out POCs to meet the requirements',
+                'Leading 4-6 members team here',
+                'Helping the teams in troubleshooting and resolving of the bugs',
+                'Code reviews of junior developers and mentoring them',
+            ]
         },
         {
             image:'./assets/images/techjockey-logo-1.svg',
@@ -72,7 +94,26 @@ const d = {
             loc:'New Delhi',
             description:'Worked as Software Developer, developed new websites, code in php framework for adding new features in projects, create apis using nodejs, libraries using javascript, integrate those with exiting project. Website debugging, code optimization, web performance along with code reviews, ',
             tenure:['Worked','10 Months'],
-            skill:['PHP','JavaScript','JQuery','HTML','LESS','CSS','NodeJs','ExpressJs','CodeIgnitor','Laravel','ReactJs','Bootstrap','Materialize','API Development','MySQL','MongoDB','Postman','JIRA','ClickUp','GIT','GitHub','Docker']
+            skill:['PHP','JavaScript','JQuery','HTML','LESS','CSS','NodeJs','ExpressJs','CodeIgnitor','Laravel','ReactJs','Bootstrap','Materialize','API Development','MySQL','MongoDB','Postman','JIRA','ClickUp','GIT','GitHub','Docker'],
+            projects:[
+                'Techjockey',
+                ' SISL Infotech Projects'
+            ],
+            points:[
+                'Experienced in dockerization of projects and run containers',
+                'Used php codeignitor framework for ecommerce platform',
+                'Added new features and services(business-logic) in existing projects',
+                'Built new libraries using javascript and added in new & existing projects',
+                'Built & integrate apis for new projects using nodejs, express and mongodb',
+                'Built & deploy nodejs applications in docker containers and give control over using pm2',
+                'Collaborate with design team & mentor interns and code reviews',
+                'Testing of apis using postman tool',
+                'Deployment of git branches using aws CI/CD pipeline on development server',
+                'jira & confluence pages use for project management',
+                'Strong debugging skills and ownership for code optimization & web performance',
+                'Carrying out POCs to meet the requirements',
+                'Resolve of the issues & bugs reported on jira/clickup',
+            ]
         },
         {
             image:'./assets/images/buzzyears-edu-logo-1.png',
@@ -81,8 +122,99 @@ const d = {
             loc:'Gurgaon',
             description:'Worked on designing of UI using HTML, CSS, JavaScript, and JQuery functions, write js functions, create calculation workflow & data pipelines using internal application. Collect client requirement and worked on jira. Create report using SQL queries and intergrate with internal application, ',
             tenure:['Worked','1 Year 6 Months'],
-            skill:['JavaScript','JQuery','HTML','CSS','Bootstrap','MySQL','JIRA','Excel','Google Spreadsheet','Data Analysis']
+            skill:['JavaScript','JQuery','HTML','CSS','Bootstrap','MySQL','JIRA','Excel','Google Spreadsheet','Data Analysis'],
+            projects:[
+                'Skolaro ERP Project',
+            ],
+            points:[
+                'Worked on design UI using HTML, CSS and JavaScript & jquery functions',
+                'Write js functions(business logic) used in internal applications',
+                'Write js interfaces for classes for creating calculation workflows used in report',
+                'Work on JIRA application for the SDLC of the project',
+                'Write SQL queries for dynamic report on mysql server',
+                'Write business logics for upcoming features in new projects and planned timelines',
+            ]
         }
+    ],
+    p:[
+        app=[
+            {
+                name:'Taxol',
+                type:'Android App',
+                details:'Deployed on Google PlayStore App',
+                app_link:'https://play.google.com/store/apps/details?id=com.androgrowtech.taxol',
+                tools_tech:'Android Studio, Java, Firebase, HTML, CSS, JavaScript, REST API, JSON',
+                status:'Active',
+                repo_link:'',
+                app_status:'Monthly 30-50 Users & 1K+ Installs',
+                hcolor:'#ea3',
+                points:[
+                    'Will add soon!'
+                ]
+            },
+            {
+                name:'GSPlus',
+                type:'Android App',
+                details:'Deployed on Google PlayStore App',
+                app_link:'https://play.google.com/store/apps/details?id=com.androgrowtech.gsplus',
+                tools_tech:'Android Studio, Java, HTML, CSS, JavaScript',
+                status:'Active',
+                repo_link:'',
+                app_status:'5+ Installs',
+                hcolor:'#ea3',
+                points:[
+                    'Will add soon!'
+                ]
+            },
+        ],
+        desktop=[
+            {
+                name:'Student Management System',
+                type:'Desktop Application',
+                details:'Build for manage the students in school. Focus majorly on student attendence, assessment, quiz, marks records.',
+                app_link:'',
+                tools_tech:'Java Swing, Netbeans, HTML, CSS, JavaScript',
+                status:'',
+                repo_link:'',
+                app_status:'',
+                hcolor:'#06c',
+                points:[
+                    'Will add soon!'
+                ]
+            },
+        ],
+        webapp=[
+            {
+                name:'OSN Services',
+                type:'Website & Webapp',
+                details:'Build for manage the students in school. Focus majorly on student attendence, assessment, quiz, marks records.',
+                app_link:'http://osnservices.in/',
+                tools_tech:'PHP, HTML, CSS, JavaScript, Codeignitor, Mysql, phpmyadmin, VSCode',
+                status:'Active',
+                repo_link:'https://github.com/arvindkumar2575/ci-osnservices',
+                app_status:'',
+                hcolor:'#1c0',
+                points:[
+                    'Will add soon!'
+                ]
+            },
+        ],
+        repos=[
+            {
+                name:'Form Validator',
+                type:'Open library',
+                details:'Developed a form validator library using javascript',
+                app_link:'',
+                tools_tech:'HTML, CSS, JavaScript, VSCode',
+                status:'Active',
+                repo_link:'https://github.com/arvindkumar2575/form-validation',
+                app_status:'',
+                hcolor:'#178',
+                points:[
+                    'Will add soon!'
+                ]
+            },
+        ]
     ]
 
 }
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,10 +2,10 @@ let app = {}
 let templates = {}
 let data = d
 
-app.skillRandomColor = (s) => {
+app.skillRandomColor = (s,min,max) => {
     let a = {}
     s.forEach(e => {
-        a["" + e + ""] = "" + app.randomColor(1, 225) + ',' + app.randomColor(1, 225) + ',' + app.randomColor(1, 225) + ""
+        a["" + e + ""] = "" + app.randomColor(min, max) + ',' + app.randomColor(min, max) + ',' + app.randomColor(min, max) + ""
     })
     return a
 }
@@ -13,7 +13,7 @@ app.randomColor = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min
 }
 // get skill colors 
-const skills = app.skillRandomColor(data.s)
+const skills = app.skillRandomColor(data.s,150,225)
 
 // get experience skills colors 
 app.expSkills = (ms, as) => {
@@ -26,21 +26,22 @@ app.expSkills = (ms, as) => {
     return a
 }
 
-// let s1 = data.e[0].skill
-// let s2 = d.e[1].skill
-// let dddd = app.expSkills(s1, skills)
-// console.log(templates.ms(dddd))
-// console.log(app.expSkills(s2,skills))
-
 
 window.addEventListener('load', () => {
     let expDiv = document.getElementById("id-AR3312")
     let exp = data.e
     let etv = ''
+    let scount = 0
     exp.forEach(i => {
         i.skill = app.expSkills(i.skill, skills)
         i.ms = templates.ms(i.skill)
-        etv += templates.e(i)
+        let mpoints = ''
+        i.points.forEach(i =>  {
+            mpoints+=templates.li(i)
+        })
+        i.mp = templates.model(i.company_name,i.projects,'experience',mpoints)
+        etv += templates.e(scount,i)
+        scount++
     })
     expDiv.innerHTML = etv
 
@@ -48,17 +49,56 @@ window.addEventListener('load', () => {
     let ps = data.sp
     let psv = ''
     ps.forEach(i=>{
+        let min=1,max=100
+        i[i.length]="" + app.randomColor(min, max) + ',' + app.randomColor(min, max) + ',' + app.randomColor(min, max) + ""
+        // console.log(i)
         psv+=templates.eps(i)
     })
     psDiv.innerHTML=psv
+
+    let pDiv = document.getElementById("id-AR3511")
+    let p = data.p
+    let pv = ''
+    p.forEach(i => {
+        i.forEach(e => {
+            let mpoints = ''
+            e.points.forEach(o =>  {
+                mpoints+=templates.li(o)
+            })
+            e.mp = templates.model(e.name,e.type,'project',mpoints)
+            pv+=templates.p(e)
+        })
+    })
+    pDiv.innerHTML=pv
+});
+let modellayout = document.querySelector('#id-AR3611')
+let modelbody = document.querySelector('#id-AR3611 .exp-content')
+document.addEventListener('click', function (e) {
+    let clss = e.target.className.split(' ')
+    if (clss.includes('showpoints')) {
+        let d = e.target.dataset.selector
+        let modelpoints = e.target.closest("div." + d).querySelector(".modelpoints")
+        modelbody.innerHTML = modelpoints.innerHTML
+        modellayout.style.display = 'block'
+    }
+
+    if(clss.includes('mclose')){
+        modelbody.innerHTML = ''
+        modellayout.style.display = 'none'
+    }
+
+    if(clss.includes('resume-download')){
+        
+    }
 });
 
 
-templates.e = (d) => {
+
+templates.e = (count,d) => {
     return `
-    <div class="col-md-12 experience-row-JX">
+    <div class="col-md-12 experience-row-JX row-${count}">
         <div class="row">
-            <div class="col-md-12 experience-JX">
+            <div class="col-md-12 experience-JX AR3333-JX">
                 <div class="row">
                     <div class="col-md-12">
                         <div class="media">
@@ -70,7 +110,10 @@ templates.e = (d) => {
                                 </h6>
                                 <div class="company-name-location-JX"><span>${d.company_name} - ${d.loc}</span>
                                 </div>
-                                <div class="work-description-JX"><span class="font-weight-bold">Description</span> : ${d.description}<a href="#">read more...</a></div>
+                                <div class="work-description-JX">
+                                    <span class="font-weight-bold">Description</span> : ${d.description}<span class="read-more showpoints" data-selector="AR3333-JX">read more...</span>
+                                </div>
+                                <div class="modelpoints">${d.mp}</div>
                             </div>
                         </div>
                     </div>
@@ -97,10 +140,55 @@ templates.eps = (d) => {
     <div class="col-sm-6 col-xs-12">
         <label>${d[0]}</label>
         <div class="progress">
-            <div class="progress-bar bg-${d[1]<50?'danger':d[1]<76?'info':d[1]<90?'primary':'success'}" role="progressbar" style="width: ${d[1]}%" aria-valuenow="35"
+            <div class="progress-bar "  role="progressbar" style="width: ${d[1]}%;background-color:rgb(${d[2]})" aria-valuenow="35"
                 aria-valuemin="0" aria-valuemax="100">${d[1]}%</div>
         </div>
     </div>`
+}
+templates.p = (d) => {
+    return `
+    <div class="col-md-6">
+        <div class="AR3512-JX AR3333-JX">
+            <div class="prj-box prj-playstore-app">
+                <div class="prj-name" style="background-color:${d.hcolor}">
+                    <h4>${d.name}</h4>
+                    <p class="prj-type">${d.type}</p>
+                </div>
+                <div class="prj-detail">
+                    <p class="detail-1">
+                        ${d.details} ${d.app_link!=''?'<a href="'+d.app_link+'">link</a>':''}
+                    </p>
+                    <div class="detail-2">
+                        <p class="font-weight-bold text-decoration-underline">Technology</p>
+                        <p>${d.tools_tech}</p>
+                        <p>${d.app_status}</p>
+                    </div>
+                </div>
+                <div class="prj-point-link">
+                    <i class="fa fa-external-link showpoints" data-selector="AR3333-JX" aria-hidden="true"></i>
+                </div>
+                <div class="modelpoints">${d.mp}</div>
+            </div>
+        </div>
+    </div>`
+}
+templates.model = (a_name,p_name,type,points) => {
+    return `
+    <div class="content-body">
+        <h4>${a_name}</h4>
+        <div class="exp-heading">
+            <h5>${p_name}</h5>
+        </div>
+        <div class="exp-body">
+            <p>${type=='experience'?'Below my job duties & responsiblities here:-':(type=='project'?'Below points used in project development:-':'')}</p>
+            <ul>
+                ${points}
+            </ul>
+        </div>
+    </div>`
+}
+templates.li = (d) => {
+    return `<li>${d}</li>`
 }
 
 
